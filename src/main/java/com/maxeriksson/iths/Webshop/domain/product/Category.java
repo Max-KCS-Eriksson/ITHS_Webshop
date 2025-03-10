8b504c11e728ef7cbd2159350d10cc0e313f10eb
Max Eriksson <teratoscincus@hotmail.com>
2025-03-10 13:46:31 +0100

    Create `Category` entity class

---
+++ src/main/java/com/maxeriksson/iths/Webshop/domain/product/Category.java
@@ -0,0 +1,29 @@
+package com.maxeriksson.iths.Webshop.domain.product;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "categories")
+public class Category {
+
+    @Id
+    @Column(name = "name")
+    private String name;
+
+    public Category() {}
+
+    public Category(String name) {
+        setName(name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
