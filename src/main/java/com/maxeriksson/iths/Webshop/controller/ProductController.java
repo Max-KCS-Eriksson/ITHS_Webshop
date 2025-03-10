5504a653e30f2972d11d6909ae9d4bda8efbe00f
Max Eriksson <teratoscincus@hotmail.com>
2025-03-10 18:53:06 +0100

    `/products` view with all `Product` and `Category`

--- src/main/java/com/maxeriksson/iths/Webshop/controller/ProductController.java
+++ src/main/java/com/maxeriksson/iths/Webshop/controller/ProductController.java
@@ -1,8 +1,27 @@
 package com.maxeriksson.iths.Webshop.controller;
 
+import com.maxeriksson.iths.Webshop.service.ProductService;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/products")
-public class ProductController {}
+public class ProductController {
+
+    @Autowired private ProductService service;
+
+    private String viewPackage = "product/";
+    private String productListView = viewPackage + "product_list";
+
+    @GetMapping
+    public String getAll(Model model) {
+        model.addAttribute("categories", service.getAllCategories());
+        model.addAttribute("products", service.getAllProducts());
+
+        return productListView;
+    }
+}
