926af99791f5686576e75e80000ec4e049df3812
Max Eriksson <teratoscincus@hotmail.com>
2025-03-10 20:58:27 +0100

    List `Product` by `Category` in `/products/{id}`

--- src/main/java/com/maxeriksson/iths/Webshop/controller/ProductController.java
+++ src/main/java/com/maxeriksson/iths/Webshop/controller/ProductController.java
@@ -1,12 +1,16 @@
 package com.maxeriksson.iths.Webshop.controller;
 
+import com.maxeriksson.iths.Webshop.domain.product.Category;
 import com.maxeriksson.iths.Webshop.service.ProductService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/products")
@@ -21,6 +25,18 @@ public class ProductController {
     public String getAll(Model model) {
         model.addAttribute("categories", service.getAllCategories());
         model.addAttribute("products", service.getAllProducts());
+
+        return productListView;
+    }
+
+    @GetMapping("/{category}")
+    public String getProductsByCategory(
+            Model model, @PathVariable("category") String categoryName) {
+        model.addAttribute("categories", service.getAllCategories());
+        Optional<Category> category = service.getCategory(categoryName);
+        if (category.isPresent()) {
+            model.addAttribute("products", service.getProductsBy(category.get()));
+        }
 
         return productListView;
     }
