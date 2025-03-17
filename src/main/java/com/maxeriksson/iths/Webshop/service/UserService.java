78dcb07359ccfbf08a7eaa38b0ee44c2e7b93e63
Max Eriksson <teratoscincus@hotmail.com>
2025-03-17 15:46:56 +0100

    Hash `User.password` when registering new `User`

--- src/main/java/com/maxeriksson/iths/Webshop/service/UserService.java
+++ src/main/java/com/maxeriksson/iths/Webshop/service/UserService.java
@@ -1,8 +1,10 @@
 package com.maxeriksson.iths.Webshop.service;
 
+import com.maxeriksson.iths.Webshop.domain.user.User;
 import com.maxeriksson.iths.Webshop.repository.user.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.ApplicationScope;
 
@@ -11,4 +13,16 @@ import org.springframework.web.context.annotation.ApplicationScope;
 public class UserService {
 
     @Autowired private UserRepository repository;
+    @Autowired private PasswordEncoder passwordEncoder;
+
+    public User registerNewUser(User user) {
+        String email = user.getEmail();
+        boolean emailExists = repository.findById(email).isPresent();
+        if (emailExists) {
+            throw new RuntimeException("An account with this email is already registered" + email);
+        }
+
+        return repository.save(
+                new User(email, passwordEncoder.encode(user.getPassword()), user.isAdmin()));
+    }
 }
