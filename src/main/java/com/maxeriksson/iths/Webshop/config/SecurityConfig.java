641ff1b1725cf5e07349d4e7f4f5c3c32f8ea45b
Max Eriksson <teratoscincus@hotmail.com>
2025-03-18 20:11:16 +0100

    Refactor `PasswordEncoder` bean to prevent circular dependency

--- src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
+++ src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
@@ -1,15 +1,14 @@
 package com.maxeriksson.iths.Webshop.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
+    @Autowired private UserDetailsService userDetailsService;
+    @Autowired private PasswordEncoder passwordEncoder;
 }
