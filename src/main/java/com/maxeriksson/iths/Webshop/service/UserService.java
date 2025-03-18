76937f2764544f9554bf17b7a9caa8df856ea11d
Max Eriksson <teratoscincus@hotmail.com>
2025-03-18 19:58:51 +0100

    `UserService` implements `UserDetailsService`

--- src/main/java/com/maxeriksson/iths/Webshop/service/UserService.java
+++ src/main/java/com/maxeriksson/iths/Webshop/service/UserService.java
@@ -4,13 +4,16 @@ import com.maxeriksson.iths.Webshop.domain.user.User;
 import com.maxeriksson.iths.Webshop.repository.user.UserRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-import org.springframework.web.context.annotation.ApplicationScope;
+
+import java.util.Optional;
 
 @Service
-@ApplicationScope
-public class UserService {
+public class UserService implements UserDetailsService {
 
     @Autowired private UserRepository repository;
     @Autowired private PasswordEncoder passwordEncoder;
@@ -24,5 +27,14 @@ public class UserService {
 
         return repository.save(
                 new User(email, passwordEncoder.encode(user.getPassword()), user.isAdmin()));
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Optional<User> optional = repository.findById(email);
+        if (optional.isEmpty()) {
+            throw new UsernameNotFoundException("User with email " + email + " not found");
+        }
+        return optional.get();
     }
 }
