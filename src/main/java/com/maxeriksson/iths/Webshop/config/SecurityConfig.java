f2c14f1b8eba83dc65d5f11cd0914dd1614e51cc
Max Eriksson <teratoscincus@hotmail.com>
2025-03-19 14:10:52 +0100

    Add `@Bean`s for authentication

--- src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
+++ src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
@@ -3,12 +3,42 @@ package com.maxeriksson.iths.Webshop.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationEventPublisher;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Arrays;
 
 @Configuration
 public class SecurityConfig {
 
     @Autowired private UserDetailsService userDetailsService;
     @Autowired private PasswordEncoder passwordEncoder;
+
+    @Bean
+    AuthenticationManager authenticationManager() {
+        ProviderManager authProviderManager =
+                new ProviderManager(Arrays.asList(daoAuthenticationProvider()));
+        authProviderManager.setAuthenticationEventPublisher(authenticationEventPublisher());
+
+        return authProviderManager;
+    }
+
+    @Bean
+    public DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder);
+        provider.setUserDetailsService(userDetailsService);
+
+        return provider;
+    }
+
+    @Bean
+    public AuthenticationEventPublisher authenticationEventPublisher() {
+        return new DefaultAuthenticationEventPublisher();
+    }
 }
