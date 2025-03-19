8a814bc1ec1874e3196457b11572877ab875937b
Max Eriksson <teratoscincus@hotmail.com>
2025-03-19 14:11:55 +0100

    Add request authority checks

--- src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
+++ src/main/java/com/maxeriksson/iths/Webshop/config/SecurityConfig.java
@@ -3,13 +3,17 @@ package com.maxeriksson.iths.Webshop.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationEventPublisher;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.DefaultAuthenticationEventPublisher;
 import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.Arrays;
 
@@ -18,6 +22,19 @@ public class SecurityConfig {
 
     @Autowired private UserDetailsService userDetailsService;
     @Autowired private PasswordEncoder passwordEncoder;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(
+                        auth -> {
+                            auth.requestMatchers(HttpMethod.GET, "/").permitAll();
+                            auth.requestMatchers(HttpMethod.POST, "/register").permitAll();
+                            auth.anyRequest().authenticated();
+                        })
+                .formLogin(Customizer.withDefaults());
+
+        return http.build();
+    }
 
     @Bean
     AuthenticationManager authenticationManager() {
