d2b82e4a53bfe6d637ef677526e12dbd5305c17e
Max Eriksson <teratoscincus@hotmail.com>
2025-03-28 16:40:19 +0100

    Add `OrderLine` to Shopping Basket
    Change datatype of `ShoppingBasketService.products` to `Map` to prevent
    `Product` duplicates in the Shopping Basket.

--- src/main/java/com/maxeriksson/iths/Webshop/service/ShoppingBasketService.java
+++ src/main/java/com/maxeriksson/iths/Webshop/service/ShoppingBasketService.java
@@ -1,13 +1,15 @@
 package com.maxeriksson.iths.Webshop.service;
 
 import com.maxeriksson.iths.Webshop.domain.order.OrderLine;
+import com.maxeriksson.iths.Webshop.domain.product.Product;
 import com.maxeriksson.iths.Webshop.repository.order.OrderRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.context.annotation.SessionScope;
 
-import java.util.List;
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 @SessionScope
@@ -15,5 +17,13 @@ public class ShoppingBasketService {
 
     @Autowired private OrderRepository orderRepository;
 
-    private List<OrderLine> products;
+    private Map<Product, OrderLine> products = new HashMap<>();
+
+    public Map<Product, OrderLine> getAllProducts() {
+        return products;
+    }
+
+    public void addProduct(OrderLine orderLine) {
+        products.put(orderLine.getProduct(), orderLine);
+    }
 }
