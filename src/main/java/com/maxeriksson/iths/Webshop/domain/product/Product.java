f37765fdfeb355d981c8f058c9b1add2b9fd4d06
Max Eriksson <teratoscincus@hotmail.com>
2025-03-28 16:43:14 +0100

    Fix implement `hashCode` and `equals` where needed

--- src/main/java/com/maxeriksson/iths/Webshop/domain/product/Product.java
+++ src/main/java/com/maxeriksson/iths/Webshop/domain/product/Product.java
@@ -59,4 +59,28 @@ public class Product {
     public void setCategory(Category category) {
         this.category = category;
     }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((category == null) ? 0 : category.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null) return false;
+        if (getClass() != obj.getClass()) return false;
+        Product other = (Product) obj;
+        if (name == null) {
+            if (other.name != null) return false;
+        } else if (!name.equals(other.name)) return false;
+        if (category == null) {
+            if (other.category != null) return false;
+        } else if (!category.equals(other.category)) return false;
+        return true;
+    }
 }
