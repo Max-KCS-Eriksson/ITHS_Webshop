e9cfbb614f9746b6e2190c1b73aa0265af84f90c
Max Eriksson <teratoscincus@hotmail.com>
2025-03-28 16:55:10 +0100

    Create `RequestMappings` to show, & add to, Shopping Basket

--- src/main/java/com/maxeriksson/iths/Webshop/controller/ShoppingBasketController.java
+++ src/main/java/com/maxeriksson/iths/Webshop/controller/ShoppingBasketController.java
@@ -21,7 +21,34 @@ import java.util.Optional;
 public class ShoppingBasketController {
 
     @Autowired private ShoppingBasketService shoppingBasket;
+    @Autowired private ProductService productService;
 
     private String viewPackage = "order/";
     private String shoppingBasketView = viewPackage + "shopping_basket";
+
+    @GetMapping
+    public ModelAndView getShoppingBasketView(ModelMap model) {
+        String view = shoppingBasketView;
+        model.addAttribute("products", shoppingBasket.getAllProducts().values());
+        model.addAttribute("categories", productService.getAllCategories());
+
+        return new ModelAndView(view, model);
+    }
+
+    @PostMapping("/add")
+    public ModelAndView addProduct(
+            ModelMap model,
+            @RequestParam("product_id") String productId,
+            @RequestParam int quantity) {
+        Optional<Product> product = productService.getProduct(productId);
+        if (product.isPresent()) {
+            shoppingBasket.addProduct(new OrderLine(product.get(), quantity));
+        }
+
+        String view = "redirect:" + this.getClass().getAnnotation(RequestMapping.class).value()[0];
+        model.addAttribute("products", shoppingBasket.getAllProducts().values());
+        model.addAttribute("categories", productService.getAllCategories());
+
+        return new ModelAndView(view, model);
+    }
 }
