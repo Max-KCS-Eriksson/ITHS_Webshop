547a9cbd7a24602022fc48004fb82a4c1052787d
Max Eriksson <teratoscincus@hotmail.com>
2025-03-28 16:49:59 +0100

    Change `service` field name to `shoppingBasket`

--- src/main/java/com/maxeriksson/iths/Webshop/controller/ShoppingBasketController.java
+++ src/main/java/com/maxeriksson/iths/Webshop/controller/ShoppingBasketController.java
@@ -1,14 +1,24 @@
 package com.maxeriksson.iths.Webshop.controller;
 
+import com.maxeriksson.iths.Webshop.domain.order.OrderLine;
+import com.maxeriksson.iths.Webshop.domain.product.Product;
+import com.maxeriksson.iths.Webshop.service.ProductService;
 import com.maxeriksson.iths.Webshop.service.ShoppingBasketService;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/shopping-basket")
 public class ShoppingBasketController {
 
-    @Autowired private ShoppingBasketService service;
+    @Autowired private ShoppingBasketService shoppingBasket;
 }
