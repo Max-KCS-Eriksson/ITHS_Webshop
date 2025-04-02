7938f61f85d70614aaf41c731300d70728700b74
Max Eriksson <teratoscincus@hotmail.com>
2025-04-02 12:37:39 +0200

    Create `OrderService` with basic functionality

---
+++ src/main/java/com/maxeriksson/iths/Webshop/service/OrderService.java
@@ -0,0 +1,27 @@
+package com.maxeriksson.iths.Webshop.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+
+import com.maxeriksson.iths.Webshop.domain.order.Order;
+import com.maxeriksson.iths.Webshop.repository.order.OrderRepository;
+
+@Service
+@ApplicationScope
+public class OrderService {
+
+    @Autowired OrderRepository orderRepository;
+
+    public List<Order> getAllOrdersExpedited(boolean expedited) {
+        return orderRepository.findByExpedited(expedited);
+    }
+
+    public Order markOrderAsExpedited(Order order) {
+        order.setExpedited(true);
+
+        return orderRepository.save(order);
+    }
+}
