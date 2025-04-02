9117f0d82c2ce19385a317cd7f07cc45228dd68f
Max Eriksson <teratoscincus@hotmail.com>
2025-04-02 14:48:28 +0200

    Add request mappings to list, show, and delete products

--- src/main/java/com/maxeriksson/iths/Webshop/controller/AdminController.java
+++ src/main/java/com/maxeriksson/iths/Webshop/controller/AdminController.java
@@ -1,14 +1,21 @@
 package com.maxeriksson.iths.Webshop.controller;
 
+import com.maxeriksson.iths.Webshop.domain.product.Category;
+import com.maxeriksson.iths.Webshop.domain.product.Product;
+import com.maxeriksson.iths.Webshop.service.OrderService;
+import com.maxeriksson.iths.Webshop.service.ProductService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
-import com.maxeriksson.iths.Webshop.service.ProductService;
-import com.maxeriksson.iths.Webshop.service.OrderService;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/admin")
@@ -19,9 +26,47 @@ public class AdminController {
 
     private String viewPackage = "admin/";
     private String adminPanelView = viewPackage + "admin_panel";
+    private String productsPanelView = viewPackage + "product_list";
+    private String productDetailPanelView = viewPackage + "product_detail";
 
     @GetMapping
     public ModelAndView adminPanel() {
         return new ModelAndView(adminPanelView);
+    }
+
+    @GetMapping("/products")
+    public ModelAndView productsPanel(ModelMap model) {
+        model.addAttribute("categories", productService.getAllCategories());
+        model.addAttribute("products", productService.getAllProducts());
+
+        String view = productsPanelView;
+
+        return new ModelAndView(view, model);
+    }
+
+    @GetMapping("/products/{id}")
+    public ModelAndView productDetailPanel(ModelMap model, @PathVariable String id) {
+        model.addAttribute("categories", productService.getAllCategories());
+        Optional<Product> product = productService.getProduct(id);
+        if (product.isPresent()) {
+            model.addAttribute("product", product.get());
+        }
+
+        String view = productDetailPanelView;
+
+        return new ModelAndView(view, model);
+    }
+
+    @PostMapping("/products/delete")
+    public ModelAndView deleteProduct(
+            ModelMap model, @RequestParam("product_id") String productId) {
+        Optional<Product> product = productService.getProduct(productId);
+        if (product.isPresent()) {
+            productService.delete(product.get());
+        }
+
+        String view = "redirect:/products";
+
+        return new ModelAndView(view);
     }
 }
