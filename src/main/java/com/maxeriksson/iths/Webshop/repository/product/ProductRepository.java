b8bd16935413d670be2086ec10e4d9490a0f59a6
Max Eriksson <teratoscincus@hotmail.com>
2025-04-04 13:08:15 +0200

    `ProductRepository` can group `Product` by sale status

--- src/main/java/com/maxeriksson/iths/Webshop/repository/product/ProductRepository.java
+++ src/main/java/com/maxeriksson/iths/Webshop/repository/product/ProductRepository.java
@@ -12,4 +12,6 @@ public interface ProductRepository extends JpaRepository<Product, String> {
     List<Product> findByCategory(Category category);
 
     List<Product> findByNameContaining(String name);
+
+    List<Product> findByIsForSale(boolean isForSale);
 }
